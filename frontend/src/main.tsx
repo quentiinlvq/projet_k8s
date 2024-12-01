9187d266966997ffd84cd80fe4fd328dd6a0af27
quentiinlvq <qleveque21@gmail.com>
2024-12-01 01:38:08 +0100

    WIP for a beautiful front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Blog from "./Blog.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Blog />
   </StrictMode>,
 )
