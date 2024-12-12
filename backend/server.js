1457c8e1a06a3a8c7683a13d8b68a26c2a3774b8
quentiinlvq <qleveque21@gmail.com>
2024-12-12 14:02:35 +0100

    clean cors policies

--- backend/server.js
+++ backend/server.js
@@ -10,27 +10,7 @@ const articlesRouter = require('./routes/articlesRoute')
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            /^http:\/\/127\.0\.0\.1:\d+$/,
-            /^http:\/\/localhost:\d+$/,
-        ];
-
-        if (allowedOrigins.some(pattern => pattern.test(origin))) {
-            callback(null, true);
-        } else {
-            callback(new Error('Non autorisé par CORS'), false);
-        }
-    }
-}));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors());
 
 sequelize.authenticate()
     .then(() => {
